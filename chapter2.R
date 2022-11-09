ec6f111643118211c99422b18f986ef37d3ee332
RobertJohnston <rfjohnstonunicef@gmail.com>
2022-11-09 19:32:20 +0100

    updates to Ch2

--- chapter2.R
+++ chapter2.R
@@ -16,18 +16,38 @@ help(sqrt)
 # FIND CODE FOR FOLLOWING
 # average (a tricky one; this is the arithmetic mean)
 base::mean		Arithmetic Mean
+
 # std (another tricky one; this is the standard deviation)
 stats::sd		Standard Deviation
+
 # match (find a particular value in a column of cells, and return its position)
 match(x, table, nomatch = NA_integer_, incomparables = NULL)
+
 # sum
 sum()
+
 # min
 min()
+
+# help files
+help(sqrt)
+help.search("meatloaf")
+??meatloaf
 
 # Plot
 x <- sort(rnorm(47))
 plot(x, type="s", main="plot(x, type=\"s\")")
 
+# check out other suggested plots
+example(plot)
 
+# store the result of sqrt(10) as result
+result <- sqrt(10)
+
+result
+
+get('result', envir=globalenv())
+
+# list all variables in globalenv()
+ls()
 
