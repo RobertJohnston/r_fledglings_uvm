e6bfc7af3fe2109b63e9f4b2dc5163ee8a73284b
RobertJohnston <rfjohnstonunicef@gmail.com>
2022-11-09 19:53:03 +0100

    add seq()

---
+++ chapter3.R
@@ -0,0 +1,19 @@
+# Functions
+pi 
+
+args(sqrt)
+
+sqrt(x=100)
+
+help(citation)
+citation()
+
+help(round)
+args(round)
+
+round(x= pi, digits = 2)
+round(x= pi)
+round(pi, 2)
+
+# rules in the tidyverse style guide
+# https://style.tidyverse.org/index.html
