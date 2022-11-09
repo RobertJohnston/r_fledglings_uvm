ab40d07de72727285df849db608564e3f5c26dee
RobertJohnston <rfjohnstonunicef@gmail.com>
2022-11-09 16:38:29 +0100

    Begin ch 2

---
+++ chapter2.R
@@ -0,0 +1,26 @@
+# get the square root
+sqrt(10)
+# get the working directory
+getwd()
+
+# use the setwd() function to set the working directory
+# notice the forward slashes used to separate levels
+# setwd("C:/R_for_Fledglings")
+
+# choose.dir()
+# file.choose()
+
+# call up the helpfile for the sqrt function
+help(sqrt)
+
+# FIND CODE FOR FOLLOWING
+# average (a tricky one; this is the arithmetic mean)
+base::mean		Arithmetic Mean
+# std (another tricky one; this is the standard deviation)
+stats::sd		Standard Deviation
+# match (find a particular value in a column of cells, and return its position)
+match(x, table, nomatch = NA_integer_, incomparables = NULL)
+# sum
+sum()
+# min
+min()
---
+++ notes.md
@@ -0,0 +1,3 @@
+ # r_fledglings_uvm
+ Working through coursework on: 
+ https://www.uvm.edu/~tdonovan/RforFledglings/
