7ddf520a2a36aa1d7efb01db6eada002fe56b7c6
RobertJohnston <rfjohnstonunicef@gmail.com>
2022-11-10 14:51:37 +0100

    add value labels to values of variables

--- chapter4.R
+++ chapter4.R
@@ -86,3 +86,93 @@ birthday2 <- as.Date("7/18/1918", format = "%m/%d/%Y")
 # look at the structure of birthday2
 str(birthday2)
 
+# Use ISO 8601 standard. This is the default date standard in R.
+# use the year-month-day format, which is R's default
+birthday2  <- as.Date("1918-07-18")
+
+# look at the structure of birthday2
+str(birthday2)
+
+fiveBooleans <- fiveRandoms > 0.5
+fiveBooleans
+
+# look at R's built in object called characters; 
+# note that this is not in your Environment
+letters
+
+# create a new object called letters2
+# set the value to be equal to the value in the object called letters
+letters2 <- letters
+# get the structure of the object, letters2
+str(letters2)
+
+# create an object called mixed, with values "dog", 10, and TRUE
+mixed <- c("dog", 10, TRUE)
+
+# look at the object called mixed
+mixed
+
+# create an object called mixed2, with values FALSE, 10, and TRUE
+mixed2 <- c(FALSE, 10, TRUE)
+
+# look at the object called mixed2
+mixed2
+
+# find the class for the object called mixed2
+class(mixed2)
+# find the structure of the object called mixed2
+str(mixed2)
+
+# Note: An object can be fully examined by using the class function, the typeof function, and the mode function. Though not always the case, these three functions can return the same result, especially for vectors. We won’t dwell on the differences, but generally speaking typeof and mode provide information about how R is storing an object, while an object’s class determines how it is used in future R calls.
+
+# Coercion functions start with “as.” and end with the output class. Here are a few that convert objects from one type to another:
+   
+# as.character - converts to character data types
+# as.numeric - converts to numeric data types
+# as.Date - converts to dates
+# as.factor - converts characters to factors
+
+# create a character string of Eleanor Roosevelt's birthday
+eleanor  <- "1884-10-11"
+# look at the value of eleanor
+str(eleanor)
+# convert to date
+eleanor  <- as.Date(eleanor)
+# look at the value of eleanor
+str(eleanor)
+# convert to number
+eleanor <- as.numeric(eleanor)
+# look at the value of eleanor
+str(eleanor)
+
+# create a names vector of the boys in Mrs. Smith's first grade class; 
+# note how popular the name "Ben" is in Vermont
+boys <- c("Ben", "Alden", "Ben", "Ben", "Max", "Elias", "Evan", "Miles")
+
+# look at the structure; notice that this is a character vector
+str(boys)
+
+# convert the vector to factors
+boys  <-  as.factor(boys)
+
+#look at the structure, notice that this is a factor with 6 levels
+str(boys)
+
+# The levels functions returns the names of each level.
+
+# look at the levels
+levels(boys)
+
+# convert the factor boys to integers
+as.integer(boys)
+
+# create a dataset with 50 values of numbers of 1 to 5 using the sample function; 
+data <- sample(x = 1:5, size = 50, replace = T, prob = c(0, 0.1, 0.1, 0.2, 0.6))
+
+# look at the data
+data
+
+# convert the vector to factor and control the levels and labels.
+factor(x = data, 
+       levels = c(1,2,3,4,5), 
+       labels = c("rotten", "poor", "ok", "good", "excellent"))
