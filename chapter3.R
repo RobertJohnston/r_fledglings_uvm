103a14568daff6f929c409de4e1dca596e44bb53
RobertJohnston <rfjohnstonunicef@gmail.com>
2022-11-10 13:57:17 +0100

    COmpleted CH3

--- chapter3.R
+++ chapter3.R
@@ -44,5 +44,35 @@ plot(x = xx, y = sqrt(abs(xx)),  col = "red")
 help(abs)
 help(plot)
 
-# completed up to 3.2
+# There are a few functions that you’ll use repeatedly that do not require a name, but rest assured they are still functions. Here are a few examples:
+
+# + (addition)
+# - (subtraction)
+# / (division)
+# ^ (exponentiation)
+# %% (modulus)
+# %/% (integer division)
+
+# Here are some more operators, which won’t be found on a calculator:
+
+# : (the colon operator, used to generate regular sequences, as in “x <- -9:9”)
+# :: (the double colon operator, used for specifying a package name and function)
+# ~ (the tilde operator, used to separate the left and right hand side of a model formula)
+
+# some databases are preloaded in RStudio - see environment / package:datasets
+
+# see 1st 10 cases in db ChickWeight
+head(ChickWeight, n=10)
+
+# Chapter 3 for installing packages
+
+# R Library
+library()
+.libPaths()
+
+# Establish your own site library
+R.home()
+
+
+
 
