1770b4fef39aeb4a32622a7f832fede0945ac81a
RobertJohnston <rfjohnstonunicef@gmail.com>
2022-11-10 14:23:54 +0100

    add sample function

--- chapter4.R
+++ chapter4.R
@@ -22,6 +22,41 @@ rm(result)
 # bring it back to life, this time with the assign function:
   
 # use  assign function to create  object result and whose value is 10
-  assign(x = 'result', value = 10)
+assign(x = 'result', value = 10)
+
+# create  object called lowNumbers,  make it a vector with  numbers 1, 2, 3, 5,  6
+lowNumbers  <-  c(1,2,3,5,6)
+
+# c() is combine function. 
+
+# look at the object called lowNumbers
+lowNumbers
+
+# Although the code below is overkill, it illustrates the fact that objects in R must exist in some environment or other, and that everything that R does is accomplished by a function, even if it happens behind the scenes:
+  
+get('result', envir = globalenv())
+
+# We’ve mentioned that the Environment tab is the Most Valuable Player (MVP) of RStudio. There is no faster way to learn how to code in R than to execute some code, and then look in the Environment pane and see what happened.
+
+# Let’s create a new vector called fiveRandoms which will be a vector that stores 5 random numbers between 0 and 1, drawn from a uniform distribution. In Excel, this is done by using the RAND function. In R, this function is called runif, which stands for “random uniform”. First, let’s look at the helpfile.
+
+# look at the helpfile
+help(runif)
+
+# And now we can create our new object.
+
+# create a new object called fiveRandoms, using the runif function
+fiveRandoms <- runif(n = 5)
+
+# look at the object called fiveRandoms
+fiveRandoms
+
+# Now let’s use the sample function to randomly sample 5 numbers between 1 and 100. It’s important here to look at the helpfile, and to understand each argument.
+
+# look at the helpfile
+help(sample)
+
+# look at the arguments for the sample function
+args(sample)
 
 
